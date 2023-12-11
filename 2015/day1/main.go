d33a129abe4fa556a42c110e3f916d9ee9cd097e
hrantgartley <doyouevenjump@gmail.com>
2023-12-11 16:45:11 -0600

    2015-day1

--- 2015/day1/main.go
+++ 2015/day1/main.go
@@ -1,10 +1,72 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 )
 
 func main() {
-	fmt.Println("This is main.go for Day 1")
-	// Your code here
+	part1()
+	part2()
+}
+
+func part1() {
+	file, err := os.Open("input")
+	if err != nil {
+		fmt.Println("Error opening file:", err)
+		return
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	openParen := 0
+	closeParen := 0
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		for _, char := range line {
+			switch char {
+			case '(':
+				openParen++
+			case ')':
+				closeParen++
+			}
+		}
+	}
+
+	currentFloor := openParen - closeParen
+	fmt.Println("Santa is on floor", currentFloor)
+}
+
+func part2() {
+	baseFloor := 0
+	file, err := os.Open("input")
+	if err != nil {
+		fmt.Println("Error opening file:", err)
+		return
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	position := 0 // Initialize position tracker
+	for scanner.Scan() {
+		line := scanner.Text()
+		for _, char := range line {
+			position++
+			switch char {
+			case '(':
+				baseFloor++
+			case ')':
+				baseFloor--
+			}
+			if baseFloor == -1 {
+				fmt.Println("Santa entered the basement at position", position)
+				return
+			}
+		}
+	}
+	fmt.Println("Santa never enters the basement.")
 }
