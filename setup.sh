ed707ee8865834d240f1cf538f2c5877760c95f4
hrantgartley <doyouevenjump@gmail.com>
2023-12-12 15:49:21 -0600

    more script fixes for python parsing

--- setup.sh
+++ setup.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 SESSION_COOKIE=$(<session.txt)
 DAY=$1
 YEAR=$2
@@ -9,15 +11,18 @@ else
 	exit 1
 fi
 
-mkdir -p "$DIR"
+mkdir -p "$DIR" || {
+	echo "Failed to create directory '$DIR'."
+	exit 1
+}
 
 URL="https://adventofcode.com/${YEAR}/day/${DAY}/input"
 
 DEST_INPUT="$DIR/input"
 DEST_MAIN="$DIR/main.py"
 
-	# Create main.go file
-cat <<EOM >$DEST_MAIN
+# Create main.py file
+cat <<EOM >"$DEST_MAIN"
 def part1():
     print("This is main.py for Day $DAY")
     # Your code here
@@ -31,38 +36,15 @@ if __name__ == "__main__":
     part2()
 EOM
 
-	# Create an empty input file
-	touch $DEST_INPUT
+# Download input file
+if ! curl -sS --cookie "session=$SESSION_COOKIE" "$URL" >"$DEST_INPUT"; then
+	echo "Failed to download the contents from $URL"
+	exit 1
+fi
 
-	curl -sS --cookie "session=$SESSION_COOKIE" $URL >$DEST_INPUT
-	echo "Directory 'day$DAY' created with necessary files for years 2015-2022."
-
+if [ -s "$DEST_INPUT" ]; then
+	echo "Input file '$DEST_INPUT' created successfully with the contents from $URL"
 else
-
-	curl -sS --cookie "session=$SESSION_COOKIE" $URL >$DEST_INPUT
-
-	if [ -s $DEST_INPUT ]; then
-		echo "Input file '$DEST_INPUT' created successfully with the contents from $URL"
-	else
-		echo "Failed to download the contents from $URL"
-		exit 1
-	fi
-
-	DEST_MAIN1="$DIR/main.py"
-
-cat <<EOM >$DEST_MAIN1
-def part1():
-    print("This is main.py for Day $DAY")
-    # Your code here
-
-def part2():
-    print("This is main.py for Day $DAY")
-    # Your code here
-
-if __name__ == "__main__":
-    part1()
-    part2()
-EOM
-
-
-	echo "Directory 'day$DAY' created with necessary files for 2023."
+	echo "The downloaded input file is empty or incomplete."
+	exit 1
+fi
