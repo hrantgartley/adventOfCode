4c003ed5a199ea9437890d12450195ccd30f514c
hrantgartley <doyouevenjump@gmail.com>
2023-12-15 10:37:44 -0600

    c++ added bc python ez

---
+++ 2019/day13/main.py
@@ -0,0 +1,11 @@
+def part1():
+    print("This is main.py for 2019 Day 13")
+    # Your code here
+
+def part2():
+    print("This is main.py for 2019 Day 13")
+    # Your code here
+
+if __name__ == "__main__":
+    part1()
+    part2()
---
+++ 2023/day15/main.cc
@@ -0,0 +1,43 @@
+#include <cstddef>
+#include <fstream>
+#include <iostream>
+#include <sstream>
+
+// Define your hash function (you can replace this with your specific hash
+// function)
+size_t customHash(std::string str) {
+  size_t value = 0;
+  for (int i = 0; i < str.length(); i++) {
+    value += (int)str[i];
+    value *= 17;
+    value %= 256;
+  }
+  return value;
+}
+
+int main() {
+  std::ifstream inputFile("input");
+  if (!inputFile.is_open()) {
+    std::cout << "Could not open file!" << std::endl;
+    return 1;
+  }
+
+  std::string line;
+  size_t totalHash = 0;
+
+  while (std::getline(inputFile, line)) {
+    std::stringstream ss(line);
+    std::string token;
+
+    while (std::getline(ss, token, ',')) {
+      // Apply hash function to each token
+      totalHash += customHash(token);
+    }
+  }
+
+  inputFile.close();
+
+  std::cout << "Sum of hashed values: " << totalHash << std::endl;
+
+  return 0;
+}
