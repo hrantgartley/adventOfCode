72e60c0c314c06c944de7f98d89a9098e804dec2
hrantgartley <doyouevenjump@gmail.com>
2023-12-15 19:27:10 -0600

    pt2 done file issues so seperate

--- 2023/day15/main.py
+++ 2023/day15/main.py
@@ -15,10 +15,35 @@ def part1():
 
 
 def part2():
-    print("This is main.py for 2023 Day 15")
-    print(hash_function("Hello World!"))
+    boxes = [[] for _ in range(256)]
+    focal_lengths = {}
+
+    for instruction in input().split(","):
+        if "-" in instruction:
+            label = instruction[:-1]
+            index = hash(label)
+            if label in boxes[index]:
+                boxes[index].remove(label)
+        else:
+            label, length = instruction.split("=")
+            length = int(length)
+            
+            index = hash(label)
+            if label not in boxes[index]:
+                boxes[index].append(label)
+                
+            focal_lengths[label] = length
+
+    total = 0
+
+    for box_number, box in enumerate(boxes, 1):
+        for lens_slot, label in enumerate(box, 1):
+            total += box_number * lens_slot * focal_lengths[label]
+
+    print(total)
+
 
 
 if __name__ == "__main__":
-    part1()
+    # part1()
     part2()
