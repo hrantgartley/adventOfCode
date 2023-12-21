89f2e121b3a17341109216c754fa7dadad902cf8
hrantgartley <doyouevenjump@gmail.com>
2023-12-21 15:46:29 -0600

    day21-final changes var names

--- 2023/day21/main.py
+++ 2023/day21/main.py
@@ -5,35 +5,38 @@ from typing import Set, Tuple
 def part1():
     grid = open('input').read().splitlines()
 
-    starting_row, starting_col = next((r,c) for r, 
-        row in enumerate(grid) for c, ch in enumerate(row) if ch == 'S')
+    starting_row, starting_col = next((r, c) for r,
+                                      row in enumerate(grid) for c, ch in enumerate(row) if ch == 'S')
     final: Set[Tuple[int, int]] = set()
     seen: Set[Tuple[int, int]] = {(starting_row, starting_col)}
-    final_queue: deque[Tuple[int, int, int]] = deque([(starting_row, starting_col, 64)])
+    final_queue: deque[Tuple[int, int, int]] = deque(
+        [(starting_row, starting_col, 64)])
 
     while final_queue:
         row, column, start = final_queue.popleft()
 
         if start % 2 == 0:
-            final.add((row,column))
+            final.add((row, column))
         if start == 0:
             continue
 
-        for new_row, new_col in [(row +1, column), (row-1,column), (row,column+1), (row, column-1)]:
+        for new_row, new_col in [(row + 1, column), (row-1, column), (row, column+1), (row, column-1)]:
             if new_row < 0 or new_row >= len(grid) or \
-                new_col < 0 or new_col >= len(grid[0]) or grid[new_row][new_col] == "#" \
-                or (new_row, new_col) in seen:
+                    new_col < 0 or new_col >= len(grid[0]) or grid[new_row][new_col] == "#" \
+                    or (new_row, new_col) in seen:
                 continue
-            seen.add((new_row,new_col))
+            seen.add((new_row, new_col))
             final_queue.append((new_row, new_col, start - 1))
     print(len(final))
     # Your code here
+
 
 def part2():
 
     grid = open('input').read().splitlines()
 
-    sr, sc = next((r, c) for r, row in enumerate(grid) for c, ch in enumerate(row) if ch == "S")
+    sr, sc = next((r, c) for r, row in enumerate(grid)
+                  for c, ch in enumerate(row) if ch == "S")
 
     assert len(grid) == len(grid[0])
 
@@ -46,21 +49,21 @@ def part2():
     def fill(sr, sc, ss):
         ans = set()
         seen = {(sr, sc)}
-        q = deque([(      sr, sc, ss)])
+        q = deque([(sr, sc, ss)])
 
         while q:
-            r, c, s = q.popleft()
+            row, col, start = q.popleft()
 
-            if s % 2 == 0:
-                ans.add((r, c))
-            if s == 0:
+            if start % 2 == 0:
+                ans.add((row, col))
+            if start == 0:
                 continue
 
-            for nr, nc in [(r + 1, c), (r - 1, c), (r, c + 1), (r, c - 1)]:
-                if nr < 0 or nr >= len(grid) or nc < 0 or nc >= len(grid[0]) or grid[nr][nc] == "#" or (nr, nc) in seen:
+            for new_row, new_col in [(row + 1, col), (row - 1, col), (row, col + 1), (row, col - 1)]:
+                if new_row < 0 or new_row >= len(grid) or new_col < 0 or new_col >= len(grid[0]) or grid[new_row][new_col] == "#" or (new_row, new_col) in seen:
                     continue
-                seen.add((nr, nc))
-                q.append((nr, nc, s - 1))
+                seen.add((new_row, new_col))
+                q.append((new_row, new_col, start - 1))
 
         return len(ans)
 
@@ -94,6 +97,8 @@ def part2():
         (grid_width + 1) * (small_tr + small_tl + small_br + small_bl) +
         grid_width * (large_tr + large_tl + large_br + large_bl)
     )
+
+
 if __name__ == "__main__":
     part1()
     part2()
