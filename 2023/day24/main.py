1ece3ab04291780ededecf18867df4c201e4194a
hrantgartley <doyouevenjump@gmail.com>
2024-01-04 16:01:29 -0600

    removed all sample inputs

--- 2023/day24/main.py
+++ 2023/day24/main.py
@@ -3,7 +3,8 @@ from day24_class import Hailstone
 
 
 def part1():
-    hailstones = [Hailstone(*map(int, line.replace("@", ",").split(","))) for line in open('input')]
+    hailstones = [Hailstone(*map(int, line.replace("@", ",").split(",")))
+                  for line in open('input')]
 
     total = 0
 
@@ -24,22 +25,27 @@ def part1():
 
 def part2():
     answer = None
-    hailstones = [tuple(map(int, line.replace("@", ",").split(","))) for line in open('input')]
+    hailstones = [tuple(map(int, line.replace("@", ",").split(",")))
+                  for line in open('input')]
 
-    final_x, final_y, final_z, final_vx, final_vy, final_vz = sympy.symbols("final_x final_y final_z final_vx final_vy final_vz")
+    final_x, final_y, final_z, final_vx, final_vy, final_vz = sympy.symbols(
+        "final_x final_y final_z final_vx final_vy final_vz")
 
     position_equations = []
     final_positions = []
 
     for index, (stone_x, stone_y, stone_z, stone_vx, stone_vy, stone_vz) in enumerate(hailstones):
-        position_equations.append((final_x - stone_x) * (stone_vy - final_vy) - (final_y - stone_y) * (stone_vx - final_vx))
-        position_equations.append((final_y - stone_y) * (stone_vz - final_vz) - (final_z - stone_z) * (stone_vy - final_vy))
+        position_equations.append(
+            (final_x - stone_x) * (stone_vy - final_vy) - (final_y - stone_y) * (stone_vx - final_vx))
+        position_equations.append(
+            (final_y - stone_y) * (stone_vz - final_vz) - (final_z - stone_z) * (stone_vy - final_vy))
 
         if index < 2:
             continue
 
         solutions = sympy.solve(position_equations)
-        integer_solutions = [sol for sol in solutions if all(val % 1 == 0 for val in sol.values())] if solutions else []
+        integer_solutions = [sol for sol in solutions if all(
+            val % 1 == 0 for val in sol.values())] if solutions else []
 
         if len(integer_solutions) == 1:
             final_positions = integer_solutions
@@ -47,7 +53,8 @@ def part2():
 
     if final_positions:
         answer = final_positions[0]
-        result = answer.get(final_x, 0) + answer.get(final_y, 0) + answer.get(final_z, 0)
+        result = answer.get(final_x, 0) + \
+            answer.get(final_y, 0) + answer.get(final_z, 0)
         print(result)
 
 
